5681b059b20be6794e55eebf3d45c820f4c3ed94
HongJimin <baker0929@naver.com>
2020-12-10 21:28:35 +0900

    nodejs_study 1210 first commit

---
+++ ex1/test1.js
@@ -0,0 +1,14 @@
+var result = 0;
+console.time('TimeTest');
+for (var i = 0; i < 100; i++) {
+    result += i;
+}
+console.timeEnd('TimeTest');
+console.log(result);
+
+
+var Person = {
+    name: "크쿠루삥뽕",
+    size: 1
+};
+console.dir(Person);
